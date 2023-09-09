a5c4f8584fb3da8b4e02ba9f7b983b74e870f79a
azubair2012 <studio.zubair@gamil.com>
2023-09-09 22:00:41 +0600

    port updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config(); // to use.env file for environment variables.
+const port = process.env.PORT;
+const cors = require("cors");
 app.all("/", (req, res) => {
   console.log("Just got a request!");
   res.send("Yo!");
 });
-app.listen(process.env.PORT || 3000);
+app.listen(port || 5000);
