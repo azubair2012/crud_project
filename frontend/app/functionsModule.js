2007d555bb5bef0ffb3831438a7ed6bb5ee407bb
azubair2012 <studio.zubair@gamil.com>
2023-09-19 11:49:27 +0600

    s

--- frontend/app/functionsModule.js
+++ frontend/app/functionsModule.js
@@ -3,43 +3,43 @@
 import { useState } from "react";
 
 export function useRouteData() {
-    const [Response, setResponse] = useState();
-    const [Delete, setDelete] = useState();
-    const [Id, setId] = useState(null);
-    const [Name, setName] = useState(null);
-    const [Position, setPosition] = useState(null);
-    const [Date, setDate] = useState(null);
+  const [Response, setResponse] = useState();
+  const [Delete, setDelete] = useState();
+  const [Id, setId] = useState(null);
+  const [Name, setName] = useState(null);
+  const [Position, setPosition] = useState(null);
+  const [Date, setDate] = useState(null);
 
-    const getRoute = async() => {
-        try {
-            const response = await fetch("http://localhost:5000", {
-                method: "GET",
-                headers: {
-                    Accept: "application/json",
-                },
-            });
-            const result = await response.json();
-            setResponse(result);
-        } catch (error) {
-            alert(error);
-        }
-    };
+  const getRoute = async () => {
+    try {
+      const response = await fetch("http://localhost:5000", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const result = await response.json();
+      setResponse(result);
+    } catch (error) {
+      alert("Server not connected");
+    }
+  };
 
-    const deleteById = async(employee) => {
-        let employeeId = employee.id;
+  const deleteById = async (employee) => {
+    let employeeId = employee.id;
 
-        try {
-            const response = await fetch(`http://localhost:5000/${employeeId}`, {
-                method: "DELETE",
-            });
+    try {
+      const response = await fetch(`http://localhost:5000/${employeeId}`, {
+        method: "DELETE",
+      });
 
-            const deletedResult = await response.json();
-            setDelete(deletedResult);
-            getRoute();
-        } catch (error) {
-            console.error("Error deleting data:", error);
-        }
-    };
+      const deletedResult = await response.json();
+      setDelete(deletedResult);
+      getRoute();
+    } catch (error) {
+      console.error("Error deleting data:", error);
+    }
+  };
 
-    return { Response, getRoute, deleteById }; // Export both the state and the function
+  return { Response, getRoute, deleteById }; // Export both the state and the function
 }
